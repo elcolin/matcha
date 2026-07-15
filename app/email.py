21375e42c0bedc32e4966692793351a0b7517168
elcolin <elise.col8@gmail.com>
2026-07-15 13:37:28 +0200

    feat: added slightly more detailed error to .env missing

--- app/email.py
+++ app/email.py
@@ -1,17 +1,29 @@
 import smtplib
 import os
 from email.mime.text import MIMEText
-
+        
 def send_email(to, subject, html):
+    smtp_server = os.getenv("SMTP_SERVER")
+    if not smtp_server:
+        raise RuntimeError("Missing required environment variable: SMTP_SERVER")
+
+    smtp_port = os.getenv("SMTP_PORT", 587)
+    smtp_user = os.getenv("SMTP_USERNAME")
+    if not smtp_user:
+        raise RuntimeError("Missing required environment variable: SMTP_USERNAME")
+
+    smtp_password = os.getenv("SMTP_PASSWORD")
+    if not smtp_password:
+        raise RuntimeError("Missing required environment variable: SMTP_PASSWORD")
+
     msg = MIMEText(html, "html")
     msg["Subject"] = subject
-    msg["From"] = os.getenv("SMTP_USERNAME")
+    msg["From"] = smtp_user
     msg["To"] = to
 
-    with smtplib.SMTP(os.getenv("SMTP_SERVER"), int(os.getenv("SMTP_PORT", 587))) as server:
+    with smtplib.SMTP(smtp_server, int(smtp_port)) as server:
         server.ehlo()
         server.starttls()
         server.ehlo()
-        server.login(os.getenv("SMTP_USERNAME"), os.getenv("SMTP_PASSWORD"))
+        server.login(smtp_user, smtp_password)
         server.send_message(msg)
-        
